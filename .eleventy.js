8714cce8c26ea2c7f345390e4bfe13a41c28bd85
Marco Gallen <marco.gallen@gmail.com>
2024-08-01 10:16:10 +1000

    Added some sub title to the index.njk

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
+  //eleventyConfig.addWatchTarget("./src/sass/");
 
   return {
     dir: {
