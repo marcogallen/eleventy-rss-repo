23682f181fa630d2b22a44cf9027254a6cd11afb
Marco Gallen <marco.gallen@gmail.com>
2024-08-02 20:23:49 +1000

    Added filters and links between pages

---
+++ app/lastAccessed.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const fs = require("node:fs");
+
+const env = process.env.ELEVENTY_ENV;
+
+const d = new Date();
+
+if (env == "dev") {
+  d.setDate(d.getDate() - 30);
+}
+
+console.log(new Date(d));
+
+const time = new Date(d).getTime();
+
+try {
+  fs.writeFileSync("./src/_data/lastAccessed.js", `module.exports=${time}`);
+} catch (error) {
+  console.log(error);
+}
